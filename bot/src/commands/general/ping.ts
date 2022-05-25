f14e1d651a6660579d13eb023a1c9665baff708a
Reinhardt <contact@reinhello.tech>
2022-05-25 21:00:43 +0700

    Update command

--- bot/src/commands/general/ping.ts
+++ bot/src/commands/general/ping.ts
@@ -3,6 +3,6 @@ import { MangaCordCommand, MangaCordInterfaces } from "MangaCord-framework";
 export const command: MangaCordInterfaces.Command = {
     name: "ping",
     run: async ({ client, message }) => {
-        MangaCordCommand.pingCommand(client, message);
+        return MangaCordCommand.pingCommand(client, message);
     }
 };

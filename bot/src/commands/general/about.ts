3f0e155e6b349e95624047575bc99b0200193f7f
Reinhardt <contact@reinhello.tech>
2022-06-24 17:48:22 +0700

    Refactor bot commands

--- bot/src/commands/general/about.ts
+++ bot/src/commands/general/about.ts
@@ -2,7 +2,7 @@ import { MangaCordCommand, MangaCordInterfaces } from "MangaCord-framework";
 
 export const command: MangaCordInterfaces.Command = {
     name: "about",
-    run: async ({ client, message }) => {
-        return MangaCordCommand.aboutCommand(client, message);
+    run: async (payload) => {
+        return new MangaCordCommand(payload).aboutCommand();
     }
 };
--- bot/src/commands/manga/search.ts
+++ bot/src/commands/manga/search.ts
@@ -2,7 +2,7 @@ import { MangaCordCommand, MangaCordInterfaces } from "mangacord-framework";
 
 export const command: MangaCordInterfaces.Command = {
     name: "search",
-    run: async ({ client, message, args }) => {
-        return MangaCordCommand.searchMangaCommand(client, message, args);
+    run: async (payload) => {
+        return new MangaCordCommand(payload).searchMangaCommand();
     }
 }

b9d041eb4530dbf1c8f569f37944628e9ae805d7
Reinhardt <contact@reinhello.tech>
2022-06-24 17:47:17 +0700

    Refactor logger param

--- framework/lib/events/error.ts
+++ framework/lib/events/error.ts
@@ -1,17 +1,17 @@
-import { Event } from "../interfaces";
-import { Logger } from "../util";
+import { Event } from "../Interfaces";
+import { Logger } from "../Util";
 
 export const event: Event = {
     name: "error",
     run: (client, err: string, id: number) => {
         const logger = new Logger();
 
-        logger.error({ message: err, subTitle: "MangaCordFramework::Gateway", title: `SHARD ${id ? id : "N/A"}`, type: "ERROR" });
+        client.logger.error({ console: "log", message: err, subTitle: "MangaCordFramework::Gateway", title: `SHARD ${id ? id : "N/A"}`, type: "ERROR" });
     }
 };
 
 process.on("unhandledRejection", (err: string) => {
     const logger = new Logger();
 
-    logger.error({ message: err, subTitle: "TypeScript::Error", title: "UNHANDLED REJECTION" });
+    logger.error({ console: "log", message: err, subTitle: "TypeScript::Error", title: "UNHANDLED REJECTION" });
 });

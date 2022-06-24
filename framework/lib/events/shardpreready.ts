b5f27dd34bcc2e6da65c8d6e8e5f015423642b70
Reinhardt <contact@reinhello.xyz>
2022-06-24 17:47:17 +0700

    Refactor logger param

--- framework/lib/events/shardpreready.ts
+++ framework/lib/events/shardpreready.ts
@@ -1,11 +1,8 @@
-import { Event } from "../interfaces";
-import { Logger } from "../util";
+import { Event } from "../Interfaces";
 
 export const event: Event = {
     name: "shardPreReady",
     run: async (client, id: number) => {
-        const logger = new Logger();
-
-        logger.system({ message: `Shard ${id} Connected`, subTitle: "MangaCordFramework::Gateway", title: "SHARD", type: "SYSTEM" });
+        client.logger.system({ console: "log", message: `Shard ${id} Connected`, subTitle: "MangaCordFramework::Gateway", title: "SHARD", type: "SYSTEM" });
     }
 };

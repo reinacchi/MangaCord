9a9298f2511046265d75d872f5dbebdfacd6e6a5
Reinhardt <contact@reinhello.tech>
2022-06-25 09:40:26 +0700

    Add TSDoc and rename `RichEmbed#setColor` to `RichEmbed#setColour`

--- framework/lib/Commands/Modules/AboutCommand.ts
+++ framework/lib/Commands/Modules/AboutCommand.ts
@@ -2,10 +2,16 @@ import { MangaCordClient } from "../../Client";
 import { Message, MessageReferenceReply, TextableChannel } from "eris";
 import { RichEmbed } from "../../Util";
 
-export async function aboutCommand(client: MangaCordClient, message: Message<TextableChannel>) {
+/**
+ * Sends `about` command
+ * @param client MangaCord client
+ * @param message Eris message
+ * @returns {Promise<Message<TextableChannel>>}
+ */
+export async function aboutCommand(client: MangaCordClient, message: Message<TextableChannel>): Promise<Message<TextableChannel>> {
     const embed = new RichEmbed()
         .setAuthor(client.user.username)
-        .setColor(0xED9A00)
+        .setColour(0xED9A00)
         .setDescription(client.translate("general.about.description"))
         .setTitle(client.translate("general.about.title"));
 

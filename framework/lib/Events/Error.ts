4cd822b012f50d2c7dab1d88fcf36c8effe61f78
Reinhardt <contact@reinhello.tech>
2022-06-25 08:12:44 +0700

    Remove unused variable

--- framework/lib/Events/Error.ts
+++ framework/lib/Events/Error.ts
@@ -4,8 +4,6 @@ import { Logger } from "../Util";
 export const event: Event = {
     name: "error",
     run: (client, err: string, id: number) => {
-        const logger = new Logger();
-
         client.logger.error({ console: "log", message: err, subTitle: "MangaCordFramework::Gateway", title: `SHARD ${id ? id : "N/A"}`, type: "ERROR" });
     }
 };
